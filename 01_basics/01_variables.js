c6dd844c09f2114635a99f8185cb34037cd5004e
chitranshraunak <chitranshraunak@gmail.com>
2026-08-18 19:55:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 367125 //accountId cannot be updated.
+let accountEmail = "Hgf@google.com"
+var accountPassword = 3242
+accountCity = "Kolkata" // not preferred
+let accountState //undefined
+
+//accountId = 2 //not allowed.
+
+console.log(accountId)
+
+accountEmail = "jkfn@gfg"
+accountPassword = 7789
+accountCity = "Belgium"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/ 
+
+
